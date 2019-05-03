4b35e694657f0b916f06ec85cc377b567c813379
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-03 10:01:48 +0100

    55 Video player (not working on emulator)

--- app/src/main/java/com/example/video55/MainActivity.java
+++ app/src/main/java/com/example/video55/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.video55;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // get reference to VideoView
+        final VideoView videoView =(VideoView) findViewById(R.id.videoView);
+//        String video_path =  "https://drive.google.com/open?id=1rztSxVALgAbBMTJeWuHpPdzX9JqTTWkGFA";
+        // Google drive link to mp4 won't work even on device ("Can't play this video")
+        String video_path =  "http://techslides.com/demos/sample-videos/small.mp4";
+
+        videoView.setVideoPath(video_path);
+//        Uri uri = Uri.parse(video_path);
+//        videoView.setVideoURI(uri);
+
+        // Player controls (play, stop, pause &c)
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);      // belt & braces set'ting
+
+        videoView.start();
     }
 
     @Override
